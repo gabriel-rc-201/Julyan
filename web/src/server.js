54222d5338a66c199dac5f0c1236087541be1689
gabrieldk <ganriel201@gmail.com>
2021-01-06 11:13:48 -0300

    refatorando para juntar o backend com o frontend

--- web/src/server.js
+++ web/src/server.js
@@ -1,17 +1,25 @@
 // importando dependencias
 const express = require('express');
 const path = require('path');
-
+const pages = require('./pages')
 // iniciando o express
 const server = express();
 
 server
-// utilizando os arquivos estáticos
-.use(express.static('public'))
-// criar rota
-.get('/', (request, response) => {
-  return response.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+  // utilizando os arquivos estáticos
+  .use(express.static('public'))
+
+  // configurar template engine
+  .set('views', path.join(__dirname, "views"))
+  .set('view engine', 'hbs')
+
+  // rotas da aplicação
+  .get('/', pages.index)
+  .get('/julyanCadastros', pages.julyanCadastros)
+  .get('/julyanSolicitacoes', pages.julyanSolicitacoes)
+  .get('/logedScreen', pages.logedScreen)
+  .get('/singIn', pages.singIn)
+  .get('/singUp', pages.singUp)
 
 // ligar o servidor
 server.listen(5500);
